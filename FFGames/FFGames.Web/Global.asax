66f0d966370b064920c9d895a298df39150ca123
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-01-29 12:28:34 +0100

    Added EntityFramework (MySQL)

---
+++ FFGames/FFGames.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FFGames.Web.MvcApplication" Language="C#" %>
--- FFGames/FFGames.Web/Properties/AssemblyInfo.cs
+++ FFGames/FFGames.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("adcc6257-5b76-44f7-bf6b-12e3e700c800")]
+[assembly: Guid("6a787932-2314-4f5f-a92e-bfc6218b544a")]
 
 // Version information for an assembly consists of the following four values:
 //
