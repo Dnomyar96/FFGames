ee73fd249ca8bce273be0d50ebc2ff071d7d6e40
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-01-31 19:49:52 +0100

    Fixes

--- FFGames.Web/FFGames.Data/Migrations/Configuration.cs
+++ FFGames.Web/FFGames.Data/Migrations/Configuration.cs
@@ -5,11 +5,11 @@ namespace FFGames.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<FFGames.Data.GameContext>
+    public sealed class Configuration : DbMigrationsConfiguration<FFGames.Data.GameContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(FFGames.Data.GameContext context)
--- FFGames.Web/FFGames.Web/Global.asax.cs
+++ FFGames.Web/FFGames.Web/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FFGames.Data;
+using FFGames.Data.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +19,7 @@ namespace FFGames.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<GameContext, Configuration>());
         }
     }
 }
